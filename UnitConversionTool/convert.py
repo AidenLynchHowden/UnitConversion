f42c0eaca54d88db37a670b037dbec3a8830b4bf
Aiden Lynch <aiden.lynch@howden.com>
2024-07-11 10:35:13 +0100

    commit

--- UnitConversionTool/convert.py
+++ UnitConversionTool/convert.py
@@ -1,4 +1,14 @@
-
 def convert(value_in: float, units_from: str, units_to: str) -> float:
-    value_out = convert(value_in=input, units_from='pa', units_to='bar')
+    units_to_si = {
+        'pa': 1,
+        'bar': 1e5,
+        'psi': 6.894757, 
+    }
+    
+    if units_from not in units_to_si or units_to not in units_to_si:
+        raise ValueError("Unsupported unit")
+    
+    value_in_si = value_in * units_to_si[units_from]
+    value_out = value_in_si / units_to_si[units_to]
     return value_out
+
--- UnitConversionTool/views.py
+++ UnitConversionTool/views.py
@@ -16,3 +16,16 @@ def unit_conversion(request):
         form = UnitConversionForm()
 
     return render(request, "unit_conversion.html", {"form": form})
+
+def views(request):
+    if request.method == 'POST':
+        form = UnitConversionForm(request.POST)
+        if form.is_valid():
+            value_in = form.cleaned_data['value_in']
+            units_from = form.cleaned_data['units_from']
+            units_to = form.cleaned_data['units_to']
+            value_out = form.cleaned_data['value_out']
+    else:
+        form = UnitConversionForm()
+        
+    return render(request, 'unit_conversion.html', {'form': form})
